6d01c904ffdbc90543b8b810908cd014e4e20b2a
Savina Natalya <natali.savina.01@mail.ru>
2025-06-07 19:52:57 +0300

    Finished tasks block 6

--- app/src/main/java/com/example/myapplication/NullSafetyTasks.kt
+++ app/src/main/java/com/example/myapplication/NullSafetyTasks.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication.nullsafety
+package com.example.myapplication
 
 /**
  * Вам дан код: var s: String? = null.
@@ -7,7 +7,7 @@ package com.example.myapplication.nullsafety
  */
 fun nullSafetyTask3() {
     var s: String? = null ?: "empty"
-//    s = s ?: "empty"
+    println(s) // empty
 }
 
 /**
@@ -24,22 +24,22 @@ fun nullSafetyTask4(arg: String?): String {
  * которое выбрасывается, когда пользователь вводит что-то неверное.
  * Покажите, как его можно перехватить и обработать.
  */
-class InvalidUserInputException(message: String): Exception(message)
+class InvalidUserInputException(message: String) : Exception(message)
 
 fun validateInput(input: String) {
-    if (input.length > 10)
+    if (input.length > 10) // ограничение на длину строки, если строка длиннее 10 символов, выбрасывается исключение
         throw InvalidUserInputException("String is too long!")
 }
 
 fun nullSafetyTask5() {
+    println("Input string (max 10 characters): ")
+
     val input = readln()
     try {
         validateInput(input)
-    }
-    catch (e: InvalidUserInputException) {
-        e.printStackTrace()
-    }
-    finally {
+    } catch (e: InvalidUserInputException) {
+        e.printStackTrace() // если получено исключение, выводится стэктрейс
+    } finally {
         println("String length: ${input.length}")
     }
 }
